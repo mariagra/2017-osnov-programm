3033aeb35b18773d7ffe9d5a30b936956c2abe62
mariagra <31931544+mariagra@users.noreply.github.com>
2017-12-13 17:56:17 +0100

    Add files via upload

--- tokeniser.py
+++ tokeniser.py
@@ -7,40 +7,41 @@ index = 1
 line = sys.stdin.readline()
 # while we have not read the whole file
 while line: 
-	# print out the sentence id
-	print('# sent_id = %d' % (index))
-	# print out the original text of the line, after stripping whitespace
-	line = line.strip()
-	print ('# text = %s' % (line))
-	# replace punctuation with space + punctuation
-	line = line.replace ('.', ' . ')
-	line = line.replace (',', ' ,')
-	line = line.replace ('?', ' ?')
-	line = line.replace ('!', ' !')
-	line = line.replace (':', ' :')
-	line = line.replace (')', ' )')
-	line = line.replace ('(', '( ')
-	line = line.replace ('„', '„ ')
-	line = line.replace ('“', '“ ')
-	line = line.replace ('"', ' " ')
-	# split the line into tokens with the ' ' character
-	line=line.split(' ')
-	tokens.append(line)
-	# start counting of tokens in the line
-	token_id = 1
-	# for each of the tokens in the line
-	for token in line:
-		token = token.strip()
-		if token == '':
-			continue
-		# print out the token id, the token itself and 8 empty columns
-		print ('%d\t%s\t_\t_\t_\t_\t_\t_\t_\t_' % (token_id, token))
-		# increase counting index of tokens in the line
-		token_id += 1
-	# increase counting index of lines
-	index += 1
-	print ()
-	line = sys.stdin.readline()
+	if line != '':
+		# print out the sentence id
+		print('# sent_id = %d' % (index))
+		# print out the original text of the line, after stripping whitespace
+		line = line.strip()
+		print ('# text = %s' % (line))
+		# replace punctuation with space + punctuation
+		line = line.replace ('.', ' . ')
+		line = line.replace (',', ' ,')
+		line = line.replace ('?', ' ?')
+		line = line.replace ('!', ' !')
+		line = line.replace (':', ' :')
+		line = line.replace (')', ' )')
+		line = line.replace ('(', '( ')
+		line = line.replace ('„', '„ ')
+		line = line.replace ('“', '“ ')
+		line = line.replace ('"', ' " ')
+		# split the line into tokens with the ' ' character
+		line=line.split(' ')
+		tokens.append(line)
+		# start counting of tokens in the line
+		token_id = 1
+		# for each of the tokens in the line
+		for token in line:
+			token = token.strip()
+			if token == '':
+				continue
+			# print out the token id, the token itself and 8 empty columns
+			print ('%d\t%s\t_\t_\t_\t_\t_\t_\t_\t_' % (token_id, token))
+			# increase counting index of tokens in the line
+			token_id += 1
+		# increase counting index of lines
+		index += 1
+		print ()
+		line = sys.stdin.readline()
 	
 
 
