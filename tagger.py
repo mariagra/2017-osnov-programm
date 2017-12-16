03f07151d4651c150850c7b3c517d48318936935
mariagra <31931544+mariagra@users.noreply.github.com>
2017-12-16 16:39:19 +0100

    Add files via upload

--- tagger.py
+++ tagger.py
@@ -1,8 +1,36 @@
 import sys
 
-fd = open('model.tsv', 'r') # we are opening ofur model in the file
+fd = open('model.tsv', 'r') # we are opening our model in the file
 
-freq = 0
+
+freq_tag = ''
+freq_word_tag = {} # store the word -> tag
+freq_word_max = {} # store highest tag frequency we have seen 
+
+
+for line in fd:
+	if line[0] == '#': 
+		continue
+	line = line.strip()
+	# take the wordform (column 2)
+	row = line.split('\t')
+	tagfreq = int(row[1]) # convert row[1] to be an integer so we can compare it
+	wordform = row[3].strip()
+	tag = row[2].strip()
+	if wordform not in freq_word_tag: # if we haven't seen this word before
+		freq_word_max[wordform] = 0 # set the maximum count to 0
+	if tagfreq > freq_word_max[wordform]: # if the frequency count of this tag given this word is higher than the maximum
+		freq_word_max[wordform] = tagfreq # set the new maximum to be what we just saw
+		freq_word_tag[wordform] = tag # set the tag for this word to be the one we just saw
+
+#print(freq_word_tag)
+#print(freq_word_max)
+
+freq_tag = freq_word_tag['-'] 
+
+#print(freq_tag)
+
+
 # working with another file which will be given in a command line
 #read through lines in a file
 for line in sys.stdin.readlines():
@@ -14,18 +42,5 @@ for line in sys.stdin.readlines():
 		print (line)
 		continue
 
-	row = line.split('\t')
-	# take the wordform (column 2)
-	wordform = row[1]
 
-	for line in fd:
-		if wordform in row[4]: # I am trying to find the word from the considered text in the model
-			freq = row[0]
-
-		
 	
-for line in fd:
-	
-# freq_tag - Noun
-# freq_word_tag = {}
-# freq_word_tag['book'] = 'NOUN'
