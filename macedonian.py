cb1e2d0746c6aa16af0bf4cc4b8cad5e127a6c55
mariagra <31931544+mariagra@users.noreply.github.com>
2017-12-16 13:05:26 +0100

    Add files via upload

--- macedonian.py
+++ macedonian.py
@@ -137,33 +137,6 @@ while line:
 
 
 
-# ------------------------------------------------------------------- above is made, below is in process (actually it is the middle part)
-# Copied from transliterator and will be changed afterwars
 
-
-
-#read through lines in a file
-for line in sys.stdin.readlines():
-		line = line.strip('\n') #remove all lines
-		if line == '': #if the line is blank (sentence boundary)
-				print()
-				continue
-		if line[0] == '#': #if the line is a comment
-				print (line)
-				continue
-
-		row = line.split('\t')
-		# take the wordform (column 2)
-		transliterated = row[1]
-
-		# transliterate it
-		for c in table:
-				transliterated = transliterated.replace(c, table[c])
-		# set the 10th column to the transliterted form
-		row[9] = 'Translit=' + transliterated
-
-		#print out line separated by tabs
-		print('\t'.join(row))
-	
 
 
